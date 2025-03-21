3ccc6fb6edb8d4b60e84b4e66cbb1cb1cb44d68b
ThAPerso <thierry@accart.name>
2025-03-21 12:23:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Yes)
+    music.play(music.stringPlayable("E E E G G E E E ", 960), music.PlaybackMode.InBackground)
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
+    music.play(music.stringPlayable("E E E C C E E E ", 960), music.PlaybackMode.InBackground)
 })
 input.onButtonPressed(Button.A, function () {
     // send a string
     basic.showString("A")
 })
+function btlogger (msg: string) {
+    bluetooth.uartWriteString(msg)
+}
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     // receive a string and shot it.
     msg = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
@@ -16,13 +21,24 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
 function searchObject () {
     huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        bluetooth.uartWriteString("" + huskylens.readeBox(1, Content1.xCenter))
+        btlogger("" + huskylens.readeBox(1, Content1.xCenter))
     }
 }
 let msg = ""
+basic.showIcon(IconNames.Square)
+music.setVolume(255)
 bluetooth.startUartService()
+basic.showIcon(IconNames.SmallSquare)
 huskylens.initI2c()
+basic.showIcon(IconNames.SmallDiamond)
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
     searchObject()
 })
